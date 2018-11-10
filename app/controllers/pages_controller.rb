2b1323da384a84951ce7484248d93f0607a021ed
Matias Mascazzini <matiasmasca@gmail.com>
2018-11-10 13:14:09 -0300

    Agregando front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
+    render layout: "welcome_layout"  
+    #if current_user
+    #  redirect_to dashboard_path
+    #else
+    #  render layout: "welcome_layout"
+    #end
   end
 end
